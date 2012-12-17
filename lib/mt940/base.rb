9516e21f88dafaf695354ae4a2a4d01c6a0fc6cd
Roma Druzhinin <romeod@gmail.com>
2012-12-17 22:29:32 +0400

    Reverted to old style hashes to make gem work on 1.8.7 and make all test passes

--- lib/mt940/base.rb
+++ lib/mt940/base.rb
@@ -14,11 +14,11 @@ module MT940
 
 
     def self.process(file, type)
-      file  = File.open(file, 'r:utf-8') if file.is_a?(String)
+      file = File.open(file, 'r:utf-8') if file.is_a?(String)
       if file.is_a?(File) || file.is_a?(Tempfile)
-        first_line  = file.readline
+        first_line = file.readline
         second_line = file.readline unless file.eof?
-        klass       = determine_bank(first_line, second_line)
+        klass = determine_bank(first_line, second_line)
         file.rewind
         instance = klass.new(file)
         file.close
@@ -44,7 +44,7 @@ module MT940
     def self.determine_bank(*args)
       Dir.foreach(File.dirname(__FILE__) + '/banks/') do |file|
         if file.match(/\.rb$/)
-          klass = eval(file.gsub(/\.rb$/,'').split('_').map{|e| e.capitalize}.join)
+          klass = eval(file.gsub(/\.rb$/, '').split('_').map { |e| e.capitalize }.join)
           bank  = klass.determine_bank(*args)
           return bank if bank
         end
@@ -53,11 +53,11 @@ module MT940
     end
 
     def initialize(file)
-      @transactions = []
-      @statements = []
+      @transactions           = []
+      @statements             = []
       @statement_transactions = []
-      @bank  = self.class.to_s.split('::').last
-      @bank  = 'Unknown' if @bank == 'Base'
+      @bank                   = self.class.to_s.split('::').last
+      @bank = 'Unknown' if @bank == 'Base'
       @lines = file.readlines
     end
 
@@ -67,9 +67,9 @@ module MT940
     end
 
     def parse_tag_25
-      @line.gsub!('.','')
+      @line.gsub!('.', '')
       if @line.match(/^:\d{2}:[^\d]*(\d*)/)
-        @bank_account = $1.gsub(/^0/,'')
+        @bank_account = $1.gsub(/^0/, '')
       end
     end
 
@@ -81,18 +81,18 @@ module MT940
 
     def parse_tag_60F
       if @line.match(/^:60F:(C|D)(\d{6})([A-Z]{3})(\d{1,15}),(\d{0,2})/)
-        type = $1 == 'D' ? -1 : 1
-        @opening_balance_amount = type * BigDecimal.new("#{$4}.#{$5}")
+        type                      = $1 == 'D' ? -1 : 1
+        @opening_balance_amount   = type * BigDecimal.new("#{$4}.#{$5}")
         @opening_balance_currency = $3
-        @opening_balance_date = parse_date($2)
-        @currency = @line[12..14]
+        @opening_balance_date     = parse_date($2)
+        @currency                 = @line[12..14]
       end
     end
 
     def parse_tag_61
       if @line.match(/^:61:(\d{6})(C|D)(\d+),(\d{0,2})/)
-        type = $2 == 'D' ? -1 : 1
-        @transaction = MT940::Transaction.new(:bank_account => @bank_account, :amount => type * BigDecimal.new($3 + '.' + $4), :bank => @bank, :currency => @currency)
+        type              = $2 == 'D' ? -1 : 1
+        @transaction      = MT940::Transaction.new(:bank_account => @bank_account, :amount => type * BigDecimal.new($3 + '.' + $4), :bank => @bank, :currency => @currency)
         @transaction.date = parse_date($1)
         @statement_transactions << @transaction
         @transactions << @transaction
@@ -105,20 +105,20 @@ module MT940
         @closing_balance_date     = parse_date($2)
         @closing_balance_currency = $3
         @closing_balance_amount   = type * BigDecimal.new("#{$4}.#{$5}")
-        amount_valid              = (@opening_balance_amount + @statement_transactions.inject(0){|memo, transaction| memo + transaction.amount}) == @closing_balance_amount
+        amount_valid              = (@opening_balance_amount + @statement_transactions.inject(0) { |memo, transaction| memo + transaction.amount }) == @closing_balance_amount
         @statements << MT940::Statement.new(
-          number:                   @number,
-          sequence:                 @sequence,
-          bank:                     @bank,
-          bank_account:             @bank_account,
-          opening_balance_amount:   @opening_balance_amount,
-          opening_balance_currency: @opening_balance_currency,
-          opening_balance_date:     @opening_balance_date,
-          closing_balance_amount:   @closing_balance_amount,
-          closing_balance_currency: @closing_balance_currency,
-          closing_balance_date:     @closing_balance_date,
-          amount_valid:             amount_valid,
-          transactions:             @statement_transactions
+          :number                   => @number,
+          :sequence                 => @sequence,
+          :bank                     => @bank,
+          :bank_account             => @bank_account,
+          :opening_balance_amount   => @opening_balance_amount,
+          :opening_balance_currency => @opening_balance_currency,
+          :opening_balance_date     => @opening_balance_date,
+          :closing_balance_amount   => @closing_balance_amount,
+          :closing_balance_currency => @closing_balance_currency,
+          :closing_balance_date     => @closing_balance_date,
+          :amount_valid             => amount_valid,
+          :transactions             => @statement_transactions
         )
         @statement_transactions = []
       end
@@ -127,9 +127,9 @@ module MT940
 
     def parse_tag_86
       if @line.match(/^:86:\s?(.*)$/) && parse_tag_86?
-        @tag86 = true
+        @tag86                   = true
         @transaction.description ||= ''
-        @transaction.description += (@transaction.description == '' ? '' : ' ') + $1.gsub(/>\d{2}/,'')
+        @transaction.description += (@transaction.description == '' ? '' : ' ') + $1.gsub(/>\d{2}/, '')
         @transaction.description.strip!
         parse_contra_account
       end
@@ -141,7 +141,7 @@ module MT940
 
     def parse_line
       if @tag86
-        @transaction.description += ' ' + @line.gsub(/\n/,'').gsub(/>\d{2}\s*/,'').gsub(/\-XXX/,'').gsub(/-$/,'').strip
+        @transaction.description += ' ' + @line.gsub(/\n/, '').gsub(/>\d{2}\s*/, '').gsub(/\-XXX/, '').gsub(/-$/, '').strip
         @transaction.description.strip!
         parse_contra_account
       end
