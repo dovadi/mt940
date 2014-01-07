ee1136e2958c6e7365df3620021b4d67e3f13eeb
dovadi <frank.oxener@gmail.com>
2014-01-07 21:00:07 +0100

    Little cleanup parser

--- lib/mt940/parser.rb
+++ lib/mt940/parser.rb
@@ -8,30 +8,31 @@ module MT940
     attr_accessor :transactions
 
     def initialize(file)
-      file  = File.open(file) if file.is_a?(String) 
+      file = File.open(file) if file.is_a?(String) 
       if file.is_a?(File) || file.is_a?(Tempfile)
-
-        begin
-          bank_class = determine_bank_class(file)
-          instance   = bank_class.new(file)
-          instance.parse
-          @transactions = instance.transactions
-        rescue NoMethodError => exception
-           if exception.message == "undefined method `new' for nil:NilClass"
-            raise UnknownBank.new('Could not determine bank!') 
-          else
-            raise exception
-          end
-        end
-
+        process(file)
         file.close
       else
         raise NoFileGiven.new('No file is given!')
       end
-
     end
 
     private
+
+    def process(file)
+      begin
+        bank_class = determine_bank_class(file)
+        instance   = bank_class.new(file)
+        instance.parse
+        @transactions = instance.transactions
+      rescue NoMethodError => exception
+         if exception.message == "undefined method `new' for nil:NilClass"
+          raise UnknownBank.new('Could not determine bank!') 
+        else
+          raise exception
+        end
+      end
+    end
 
     def determine_bank_class(file)
       first_line = file.readline
