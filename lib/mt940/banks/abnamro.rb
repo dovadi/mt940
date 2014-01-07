5ad5474f74cb16594a8643ae4939ad1d61f277c0
dovadi <frank.oxener@gmail.com>
2014-01-07 20:48:20 +0100

    WIP: prepare abnamro for SEPA

--- lib/mt940/banks/abnamro.rb
+++ lib/mt940/banks/abnamro.rb
@@ -1,16 +1,34 @@
 class MT940::Abnamro < MT940::Base
 
+  private
+
   def parse_tag_61
     super(/^:61:(\d{6})\d{4}(C|D)(\d+),(\d{0,2})/)
   end
 
+  def parse_tag_86
+    if @line.match(/^:86:\s?(.*)$/)
+      @tag86 = true
+      @description = $1.gsub(/>\d{2}/,'').strip
+      parse_description
+    end
+  end
+
+  def parse_description
+    if @description[0] == "/"
+    else
+      parse_contra_account
+    end
+    @transaction.description = @description
+  end
+
   def parse_contra_account
     if @transaction
-      if @transaction.description.match(/^(GIRO)\s+(\d+)(.+)/)
+      if @description.match(/^(GIRO)\s+(\d+)(.+)/)
         @transaction.contra_account = $2.rjust(9, '000000000')
-        @transaction.description    = $3
-      elsif @transaction.description.match(/^(\d{2}.\d{2}.\d{2}.\d{3})(.+)/)
-        @transaction.description    = $2
+        @description    = $3
+      elsif @description.match(/^(\d{2}.\d{2}.\d{2}.\d{3})(.+)/)
+        @description    = $2
         @transaction.contra_account = $1.gsub('.','')
       end
     end
