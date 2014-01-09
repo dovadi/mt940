6df18aa8d31067a2ab41400e46e5e13a77720aee
dovadi <frank.oxener@gmail.com>
2014-01-09 13:28:58 +0100

    WIP: implementing rabobank after sepa except description

--- lib/mt940/banks/rabobank.rb
+++ lib/mt940/banks/rabobank.rb
@@ -3,12 +3,16 @@ class MT940::Rabobank < MT940::Base
   private
 
   def parse_tag_61
-    if @line.match(/^:61:(\d{6})(C|D)(\d+),(\d{0,2})\w{4}\w{1}(\d{9}|NONREF)(.+)$/)
+    if @line.match(/^:61:(\d{6})(C|D)(\d+),(\d{0,2})\w{4}\w{1}(\d{9}|NONREF|EREF)(.*)$/)
       type = $2 == 'D' ? -1 : 1
       @transaction = MT940::Transaction.new(:bank_account => @bank_account, :amount => type * ($3 + '.' + $4).to_f, :bank => @bank, :currency => @currency)
       @transaction.date = parse_date($1)
-      @transaction.contra_account = $5.strip
-      @transaction.contra_account_owner = $6.strip
+      if @sepa
+        @transaction.contra_account = $6.strip
+      else
+        @transaction.contra_account = $5.strip
+        @transaction.contra_account_owner = $6.strip
+      end
       @transactions << @transaction
     end
   end
@@ -23,4 +27,15 @@ class MT940::Rabobank < MT940::Base
     end
   end
 
+  def parse_tag_25
+    @line.gsub!('.','')
+    if @line.match(Regexp.new ":25:(#{MT940::IBAN_PATTERN})")
+      @bank_account = $1
+      @sepa = true
+    else
+      @bank_account = $1.gsub(/^0/,'') if @line.match(/^:\d{2}:[^\d]*(\d*)/)
+    end
+  end
+
 end
+
--- test/mt940_rabobank_test.rb
+++ test/mt940_rabobank_test.rb
@@ -2,63 +2,130 @@ require 'helper'
 
 class TestMt940Rabobank < Test::Unit::TestCase
 
-  setup do
-    file_name = File.dirname(__FILE__) + '/fixtures/rabobank.txt'
-    @transactions = MT940::Parser.new(file_name).transactions
-  end
-
-  should 'have the correct number of transactions' do
-    assert_equal 3, @transactions.size
-  end
-
-  context 'Transaction' do
-
+  context 'Before SEPA' do
     setup do
-      @transaction = @transactions.first
+      file_name = File.dirname(__FILE__) + '/fixtures/rabobank.txt'
+      @transactions = MT940::Parser.new(file_name).transactions
     end
 
-    should 'have a bank_account' do
-      assert_equal '129199348', @transaction.bank_account
+    should 'have the correct number of transactions' do
+      assert_equal 3, @transactions.size
     end
 
-    context 'Contra account' do
-      should 'be determined in case of a GIRO account' do
-        assert_equal '121470966', @transaction.contra_account
+    context 'Transaction' do
+
+      setup do
+        @transaction = @transactions.first
       end
 
-      should 'be determined in case of a regular bank' do
-        assert_equal '733959555', @transactions[1].contra_account
+      should 'have a bank_account' do
+        assert_equal '129199348', @transaction.bank_account
       end
 
-      should 'be determined in case of a NONREF' do
-        assert_equal 'NONREF', @transactions.last.contra_account
+      context 'Contra account' do
+        should 'be determined in case of a GIRO account' do
+          assert_equal '121470966', @transaction.contra_account
+        end
+
+        should 'be determined in case of a regular bank' do
+          assert_equal '733959555', @transactions[1].contra_account
+        end
+
+        should 'be determined in case of a NONREF' do
+          assert_equal 'NONREF', @transactions.last.contra_account
+        end
       end
-    end
 
-    should 'have an amount' do
-      assert_equal -1213.28, @transaction.amount
-    end
+      should 'have an amount' do
+        assert_equal -1213.28, @transaction.amount
+      end
 
-    should 'have a currency' do
-      assert_equal 'EUR', @transaction.currency
-    end
+      should 'have a currency' do
+        assert_equal 'EUR', @transaction.currency
+      end
 
-    should 'have a contra_account_owner' do
-      assert_equal 'W.P. Jansen', @transaction.contra_account_owner
-    end
+      should 'have a contra_account_owner' do
+        assert_equal 'W.P. Jansen', @transaction.contra_account_owner
+      end
 
-    should 'have a description' do
-      assert_equal 'Terugboeking NIET AKKOORD MET AFSCHRIJVING KOSTEN KINDEROPVANG JUNI 20095731', @transaction.description
-    end
+      should 'have a description' do
+        assert_equal 'Terugboeking NIET AKKOORD MET AFSCHRIJVING KOSTEN KINDEROPVANG JUNI 20095731', @transaction.description
+      end
 
-    should 'have a date' do
-      assert_equal Date.new(2011,5,27), @transaction.date
-    end
+      should 'have a date' do
+        assert_equal Date.new(2011,5,27), @transaction.date
+      end
 
-    should 'return its bank' do
-      assert_equal 'Rabobank', @transaction.bank
+      should 'return its bank' do
+        assert_equal 'Rabobank', @transaction.bank
+      end
+
     end
 
   end
+
+  context 'After SEPA' do
+    setup do
+      file_name = File.dirname(__FILE__) + '/fixtures/rabobank_sepa.txt'
+      @transactions = MT940::Parser.new(file_name).transactions
+    end
+
+    should 'have the correct number of transactions' do
+      assert_equal 12, @transactions.size
+    end
+
+    context 'Transaction' do
+
+      setup do
+        @transaction = @transactions.first
+      end
+
+      should 'have a bank_account' do
+        assert_equal 'NL50RABO0123456789', @transaction.bank_account
+      end
+
+
+      context 'Contra account' do
+        should 'be determined in case of a GIRO account' do
+          assert_equal 'NL96RBOS0523149468', @transaction.contra_account
+        end
+
+        should 'be determined in case of a regular bank' do
+          assert_equal '0663616476', @transactions[1].contra_account
+        end
+
+        should 'be determined in case of Postbank account' do
+          assert_equal 'P004500018', @transactions[4].contra_account
+        end
+      end
+
+      should 'have an amount' do
+        assert_equal -127.50, @transaction.amount
+      end
+
+      should 'have a currency' do
+        assert_equal 'EUR', @transaction.currency
+      end
+
+      should 'not be able to determine a contra_account_owner' do
+        assert !@transaction.contra_account_owner
+      end
+
+      # should 'have a description' do
+      #   assert_equal 'Terugboeking NIET AKKOORD MET AFSCHRIJVING KOSTEN KINDEROPVANG JUNI 20095731', @transaction.description
+      # end
+
+      should 'have a date' do
+        assert_equal Date.new(2013,4,3), @transaction.date
+      end
+
+      should 'return its bank' do
+        assert_equal 'Rabobank', @transaction.bank
+      end
+
+    end
+
+  end
+
 
 end
